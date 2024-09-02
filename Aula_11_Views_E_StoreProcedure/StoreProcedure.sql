885d61d4cf2fefca77c247932f4c41272f61201b
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-09-02 15:35:54 -0300

    Update StoreProcedure.sql

--- Aula_11_Views_E_StoreProcedure/StoreProcedure.sql
+++ Aula_11_Views_E_StoreProcedure/StoreProcedure.sql
@@ -1,27 +1,76 @@
-CREATE PROCEDURE sp_INS_FUNC
+CREATE DATABASE FUNCIONARIOS
+USE FUNCIONARIOS
+CREATE TABLE Funcionarios(
+CodFunc INT PRIMARY KEY IDENTITY,
+NomeFunc VARCHAR(100) NOT NULL,
+SexoFunc VARCHAR(1) NOT NULL,
+BairroFunc VARCHAR(50) NOT NULL,
+SalFunc DECIMAL(8,2) NOT NULL,
+SetorFunc VARCHAR(50) NOT NULL
+)
+GO
+
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Larissa Menezes','F','Jabaquara',1200,'Marketing')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Selma Nunes','F','Grajaú',3800,'Vendas')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Leandro Henrique','M','Socorro',2950,'RH')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Amélia Jeremias','F','Socorro',4200,'Marketing')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Cláudio Jorge Silva','M','Jabaquara',1480,'Vendas')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Luciano Souza','M','Pedreira',1000,'Vendas')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Gabriela Santos Nunes','F','Jurubatuba',4150,'')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Rafaela Vieira Jr','F','Jabaquara',700,'Marketing')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Suzana Crispim','F','Grajaú',5600,'Produção')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Sabrina Oliveira Castro','F','Pedreira',2900,'Marketing')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Jarbas Silva Nunes','M','Jurubatuba',5300,'Produção')
+GO
+INSERT INTO Funcionarios (NomeFunc,SexoFunc,BairroFunc,SalFunc,SetorFunc)
+VALUES ('Ralf Borges','M','Jabaquara',1600,'Marketing')
+GO
+
+-- Criando procedure
+CREATE PROCEDURE sp_INS_Func
 @NomeFunc VARCHAR(100),
 @Sexo VARCHAR(1),
-@BairroFunc VARCHAR(50),
-@SalFunc DECIMAL (8,2),
-@SetorFunc VARCHAR(50)
+@BairroFunc VARCHAR (50),
+@SalFunc DECIMAL (8, 2),
+@SetorFunc VARCHAR (50)
 
 AS
 INSERT INTO Funcionarios
 	VALUES (@NomeFunc, @Sexo, @BairroFunc, @SalFunc, @SetorFunc)
 
--- Só verificando o que há no banco
-SELECT * FROM FUNCIONARIOS
+-- Só verificando o que há no banco 
+SELECT * FROM Funcionarios
 
--- Executando SP, e passando os parâmetros.
-EXEC sp_INS_FUNC 'Luiz Bertonha Junior', 'M', 'Vl Pacifico', '2500', 'Informática'
-				  -- NomeFunc         SexoFunc BairroFunc     SalFunc  SetorFunc
+-- Executando SP, e passando os parâmetros 
+EXEC sp_INS_FUNC 'Luiz Bertonha', 'M', 'Vl Pacífico', 2500, 'Informática'
 
---ABRINDO SP
-SP_HELPTEXT sp_INS_FUNC
+-- Abrindo SP
+SP_HELPTEXT sp_Ins_Func
 
--- Cria SP de consulta 
-CREATE PROCEDURE sp_CONS_FUNC
-@CodFunc INT 
+-- Criando SP de consulta
+CREATE PROCEDURE sp_Cons_Func
+@CodFunc INT
 AS
 	SELECT CodFunc, NomeFunc, SalFunc
 	FROM Funcionarios
@@ -29,26 +78,3 @@ AS
 
 -- Executa a SP de consulta
 EXEC SP_CONS_FUNC '3'
-
--- Fazendo Store Procedure no Northwind
-
-USE [Northwind-pr]
-
-CREATE PROCEDURE SP_PRODUTO_CLIENTE
-@CodCli nvarchar (10)
-AS
-	SELECT *
-	FROM Clientes AS C
-	INNER JOIN PEDIDOS AS P
-		ON P.CODIGODOCLIENTE = C.CodigoDoCliente
-	INNER JOIN DetalhesDoPedido AS D
-		ON D.NumeroDoPedido = P.NumeroDoPedido
-	INNER JOIN PRODUTOS AS PR
-		ON PR.CodigoDoProduto = D.CodigoDoProduto
-	WHERE C.CodigoDoCliente = @CodCli
-
--- Seleciona tabela clientes
-SELECT * FROM CLIENTES 
-
--- Executa SP consulta produto cliente
-EXEC SP_PRODUTO_CLIENTE 'ALFKI'
